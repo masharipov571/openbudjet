78dee7c18a5727cad97d9e5ca677681c2b3a7d55
masharipov571 <masharipovolloyor8@gmail.com>
2026-08-24 16:55:52 +0500

    Add files via upload

--- config.py
+++ config.py
@@ -9,7 +9,9 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 # Strict target initiative details
 INITIATIVE_ID = os.getenv("INITIATIVE_ID", "6d1035da-d586-49aa-b344-c80bd5878c0d")
+TARGET_INITIATIVE_ID = INITIATIVE_ID
 BOARD_ID = os.getenv("BOARD_ID", "55")
+TARGET_BOARD_ID = BOARD_ID
 OPENBUDGET_BASE_URL = os.getenv("OPENBUDGET_BASE_URL", "https://new.openbudget.uz")
 
 TARGET_INITIATIVE_URL = f"{OPENBUDGET_BASE_URL}/uz/initiative-budget/active-initiatives/{BOARD_ID}/{INITIATIVE_ID}"
